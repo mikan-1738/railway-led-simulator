73b3ad287b86a1c2738d511b46e24a0e56d12eeb
mikan-1738 <souma09251738.kosisou@gmail.com>
2026-09-10 07:40:08 +0900

    Initialize state variables in state.js
    わー
    チャッピー

--- state.js
+++ state.js
@@ -1,1 +1,33 @@
+let jsonData = null;
 
+let typeId = null;
+let destinationId = null;
+let nextId = null;
+let informationId = null;
+let information2Id = null;
+let carNumberId = null;
+
+let displayMode = "normal";
+let informationMode = "destination";
+let typeMode = null;
+
+let lang = "ja";
+let showNext = false;
+
+let scene = 0;
+let sceneList = [];
+
+let typeScene = 0;
+let typeSceneList = [];
+
+let frame = 0;
+
+let ledsize = 6;
+let ledgap = 1;
+let pitch = ledsize + ledgap;
+let radius = ledsize / 2;
+
+let sizeLed = document.getElementById("led");
+
+let langIndex = 0;
+const langs = ["ja", "en"];
