084b82d42d334b34db29eaa4c3115a8c8533e968
mikan-1738 <souma09251738.kosisou@gmail.com>
2026-09-10 07:34:57 +0900

    Add constants for canvas dimensions and context
    うぇい
    チャッピー

--- constants.js
+++ constants.js
@@ -1,1 +1,6 @@
+const destinationWidth = 81;
+const matrixWidth = led.width;
+const matrixHeight = led.height;
 
+const cacheCanvas = document.createElement("canvas");
+const cacheCtx = cacheCanvas.getContext("2d");
