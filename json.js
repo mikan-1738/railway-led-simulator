6737de016abc010f7fd9633217e67925d212c8e3
mikan-1738 <souma09251738.kosisou@gmail.com>
2026-09-21 10:09:13 +0900

    Update json.js

--- json.js
+++ json.js
@@ -6,10 +6,12 @@ function getItem(categoryId, itemId) {
     const category = getCategory(categoryId);
     if (!category) return null;
 
+    // 従来の構造
     if (category.items) {
         return category.items.find(i => i.id === itemId);
     }
 
+    // groups構造
     if (category.groups) {
         for (const group of category.groups) {
             const item = group.items.find(i => i.id === itemId);
