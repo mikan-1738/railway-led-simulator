fbc43f447a4b8889dd6cf51c29fdfe7d5be5428b
mikan-1738 <souma09251738.kosisou@gmail.com>
2026-09-21 10:06:26 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,6 +1,17 @@
 const destinationWidth = 81;
 const matrixWidth = led.width;
 const matrixHeight = led.height;
+const areaLeft = 48;
+const areaRight = 128;
+const areaTop = 16;
+const areaBottom = 32;
+
+// スクロール文字専用Canvas
+const scrollTextCanvas = document.createElement("canvas");
+const scrollTextCtx = scrollTextCanvas.getContext("2d");
 
 const cacheCanvas = document.createElement("canvas");
 const cacheCtx = cacheCanvas.getContext("2d");
+
+let scrollTextWidth = 0;
+scrollX = areaRight;
