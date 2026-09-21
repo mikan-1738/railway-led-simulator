e786344be982accaad4b70ce0d52fb7f2879a78d
mikan-1738 <souma09251738.kosisou@gmail.com>
2026-09-21 10:05:42 +0900

    Update state.js

--- state.js
+++ state.js
@@ -1,14 +1,4 @@
-// ==========================================
-// Railway LED Simulator
-// state.js
-// ==========================================
-
 let jsonData = null;
-let config = null;
-
-// ------------------------------------------
-// 選択状態
-// ------------------------------------------
 
 let typeId = null;
 let destinationId = null;
@@ -17,71 +7,39 @@ let informationId = null;
 let information2Id = null;
 let lineId = null;
 let carNumberId = null;
-
-// ------------------------------------------
-// 表示モード
-// ------------------------------------------
+let scrollId = null;
 
 let displayMode = "normal";
 let informationMode = "destination";
 let typeMode = null;
-
 let lang = "ja";
 let showNext = false;
-
-// ------------------------------------------
-// シーン
-// ------------------------------------------
-
 let scene = 0;
 let sceneList = [];
-
 let typeScene = 0;
 let typeSceneList = [];
-
 let frame = 0;
-
-// ------------------------------------------
-// LED設定
-// ------------------------------------------
 
 let ledsize = 6;
 let ledgap = 1;
-
 let pitch = ledsize + ledgap;
-let pitchY = ledsize * 0.9 + ledgap;
-
 let radius = ledsize / 2;
-
-// ------------------------------------------
-// Canvas
-// ------------------------------------------
-
-let sizeLed =
-    document.getElementById("led");
+let sizeLed = document.getElementById("led");
 
 let currentMatrix = null;
 
-// ------------------------------------------
-// 言語
-// ------------------------------------------
-
 let langIndex = 0;
 
-const langs = [
-    "ja",
-    "en"
-];
+let scrollX = 128;
+let scrollTextMatrix = [];
+let scrollTimer = null;
 
-// ------------------------------------------
-// 交互表示
-// ------------------------------------------
+let scrollMatrix = null;
 
-let alternateDisplay = false;
-let alternateTimer = null;
+let clickStartScrollBtn = false;
 
-// ------------------------------------------
-// 描画タイマー
-// ------------------------------------------
+let scrollGeneration = 0;
 
-let renderTimer = null;
+let scrollWaiting = false;
+
+const langs = ["ja", "en"];
